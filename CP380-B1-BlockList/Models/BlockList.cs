e1022e03f68e8acdbc8a47858247d8670540f91f
Akhil Nallamothu <42518147+NAkhilC@users.noreply.github.com>
2021-08-08 17:17:42 -0400

    Update BlockList.cs

--- CP380-B1-BlockList/Models/BlockList.cs
+++ CP380-B1-BlockList/Models/BlockList.cs
@@ -1,11 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace CP380_B1_BlockList.Models
 {
     public class BlockList
     {
-
         public IList<Block> Chain { get; set; }
 
         public int Difficulty { get; set; } = 2;
@@ -18,43 +17,44 @@ namespace CP380_B1_BlockList.Models
 
         public void MakeFirstBlock()
         {
-            var block = new Block(DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss tt"), null, new List<Payload>());
+            var block = new Block(DateTime.Now, null, new List<Payload>());
             block.Mine(Difficulty);
             Chain.Add(block);
         }
 
         public void AddBlock(Block block)
         {
-            // TODO
-
-            var s= block.PreviousHash;
-            block.PreviousHash = s;
+            Block blockva = GetBlock();
+            block.Nonce = blockva.Nonce + 1;
+            block.PreviousHash = blockva.Hash;
+            block.Mine(this.Difficulty);
             Chain.Add(block);
+        }
+
+        public Block GetBlock()
+        {
+            //get the last value in the chain
+            return Chain[Chain.Count - 1];
         }
 
         public bool IsValid()
         {
-            // TODO
-            int co=Chain.Count;
-            var one= Chain[co - 1].Hash;
-            var two = Chain[co - 1].PreviousHash;
-            //Console.WriteLine(Chain[co - 2].Hash+"--"+Chain[co - 1].PreviousHash );
-            bool tf = true ;
-
-            for(int i=1;i<Chain.Count;i++)
+            for (int i = 1; i < Chain.Count; i++)
             {
-                if ((Chain[co - (i+1)].Hash).ToString() == (Chain[co - i].PreviousHash).ToString().TrimStart('"').TrimEnd('"'))
-                {
-                    tf = true;
-                }
-                else
+                Block firstblock = Chain[i];
+                Block last = Chain[i - 1];
+
+                if (firstblock.Hash != firstblock.CalculateHash())
                 {
                     return false;
                 }
 
+                if (firstblock.PreviousHash != last.Hash)
+                {
+                    return false;
+                }
             }
-            return tf;
-
+            return true;
         }
     }
 }
