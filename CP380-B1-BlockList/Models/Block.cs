6a3848519b12c4a6cf7c3b1279a227a6f9a036dc
Akhil Nallamothu <42518147+NAkhilC@users.noreply.github.com>
2021-08-08 17:17:22 -0400

    Update Block.cs

--- CP380-B1-BlockList/Models/Block.cs
+++ CP380-B1-BlockList/Models/Block.cs
@@ -1,6 +1,5 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
@@ -9,15 +8,17 @@ namespace CP380_B1_BlockList.Models
 {
     public class Block
     {
-
-
-        public int Nonce { get; set; }
-        public string TimeStamp { get; set; }
+        public int Nonce { get; set; } = 0;
+        public DateTime TimeStamp { get; set; }
         public string PreviousHash { get; set; }
         public string Hash { get; set; }
         public List<Payload> Data { get; set; }
 
-        public Block(string timeStamp, string previousHash, List<Payload> data)
+        public Block()
+        {
+
+        }
+        public Block(DateTime timeStamp, string previousHash, List<Payload> data)
         {
             Nonce = 0;
             TimeStamp = timeStamp;
@@ -26,129 +27,34 @@ namespace CP380_B1_BlockList.Models
             Hash = CalculateHash();
         }
 
-
         //
         // JSON serialisation:
         //   https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-how-to?pivots=dotnet-5-0
         //
         public string CalculateHash()
         {
-            var sha256 = SHA256.Create();
+            SHA256 sha256 = SHA256.Create();
             var json = JsonSerializer.Serialize(Data);
-            var finaldata = "";
-
-            if (PreviousHash == null)
-            {
-                Mine(Nonce);
-                finaldata = TimeStamp + "-" + PreviousHash + "-" + (Nonce - 1) + "-[]";
-            }
-            else
-            {
-               // Console.WriteLine("Next");
-                Mine(3);
-               // Console.WriteLine(Nonce);
-            if (PreviousHash != null)
-            {
-                 foreach (var field in Data)
-                    {
-                        var grant = "GRANT";
-                        var Buy = "BUY";
-                        var corr = grant.Equals(field.gRANT.ToString());
-                        var corr1 = Buy.Equals(field.gRANT.ToString());
-                        if (corr)
-                        {
-                            finaldata = TimeStamp + "-" + PreviousHash + "-" + (Nonce - 1) + "-" + "[{\"User\":\"" + field.v1 + "\",\"TransactionType\":2,\"Amount\":" + field.v2 + ",\"Item\":\"\"" + field.p + "}]";
-                        }
-                        else if (corr1)
-                        {
-                            finaldata = TimeStamp + "-" + PreviousHash + "-" + (Nonce - 1) + "-" + "[{\"User\":\"" + field.v1 + "\",\"TransactionType\":0,\"Amount\":" + field.v2 + ",\"Item\":\"" + field.p + "\"}]";
-                        }
-                        else
-                        {
-                            finaldata = TimeStamp + "-" + PreviousHash + "-" + (Nonce - 1) + "-" + "[{\"User\":\"" + field.v1 + "\",\"TransactionType\":1,\"Amount\":" + field.v2 + ",\"Item\":\"" + field.p + "\"}]";
-                        }
-
-                    }
-            }
-        }
 
             //
             // TODO
             //
 
-            var inputString = finaldata; // TODO
-            var inputBytes = Encoding.ASCII.GetBytes(inputString);
-            var outputBytes = sha256.ComputeHash(inputBytes);
-            var oo = Convert.ToBase64String(outputBytes);
 
-            return Convert.ToBase64String(outputBytes);
+            var input = Encoding.ASCII.GetBytes($"{TimeStamp}-{PreviousHash ?? ""}-{Data}-{Nonce}");
+            var output = sha256.ComputeHash(input);
+
+            return Convert.ToBase64String(output);
         }
 
         public void Mine(int difficulty)
         {
-            var finaldata = "";
-            var sha256 = SHA256.Create();
-            var oo="";
-            var strings = "";
-            for(int i=1;i<=difficulty;i++)
+            var cvalues = new string('C', difficulty);
+            while (this.Hash == null || this.Hash.Substring(0, difficulty) != cvalues)
             {
-                strings = strings + "C";
+                this.Nonce++;
+                this.Hash = this.CalculateHash();
             }
-            //Console.WriteLine(strings);
-           
-
-            if(PreviousHash==null)
-            {
-                Nonce = difficulty;
-                do {
-                    
-                    finaldata = TimeStamp + "-" + PreviousHash + "-" + Nonce + "-[]";
-                    var inputString = finaldata; // TODO
-                    var inputBytes = Encoding.ASCII.GetBytes(inputString);
-                    var outputBytes = sha256.ComputeHash(inputBytes);
-                    oo = Convert.ToBase64String(outputBytes);
-                    Nonce++;
-
-                } while (!oo.StartsWith("CC"));
-
-            }
-            else
-            {
-                do
-                {
-                    foreach (var field in Data)
-                    {
-                        var grant = "GRANT";
-                        var Buy = "BUY";
-                        var corr = grant.Equals(field.gRANT.ToString());
-                        var corr1 = Buy.Equals(field.gRANT.ToString());
-                        if (corr)
-                        {
-                            finaldata = TimeStamp + "-" + PreviousHash + "-" + Nonce + "-" + "[{\"User\":\"" + field.v1 + "\",\"TransactionType\":2,\"Amount\":" + field.v2 + ",\"Item\":\"\"" + field.p + "}]";
-                        }
-                        else if (corr1)
-                        {
-                            finaldata = TimeStamp + "-" + PreviousHash + "-" + Nonce + "-" + "[{\"User\":\"" + field.v1 + "\",\"TransactionType\":0,\"Amount\":" + field.v2 + ",\"Item\":\"" + field.p + "\"}]";
-                        }
-                        else
-                        {
-                            finaldata = TimeStamp + "-" + PreviousHash + "-" + Nonce + "-" + "[{\"User\":\"" + field.v1 + "\",\"TransactionType\":1,\"Amount\":" + field.v2 + ",\"Item\":\"" + field.p + "\"}]";
-                        }
-
-                    }
-                    var inputString = finaldata; // TODO
-                    var inputBytes = Encoding.ASCII.GetBytes(inputString);
-                    var outputBytes = sha256.ComputeHash(inputBytes);
-                    oo = Convert.ToBase64String(outputBytes);
-                    Nonce++;
-                } while (!oo.StartsWith(strings));
-
-                
-
-            }
-            // Console.WriteLine(TimeStamp + "--" + oo + "--" + Nonce);
-
-
         }
     }
 }
